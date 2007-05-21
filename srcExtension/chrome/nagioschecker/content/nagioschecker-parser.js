2b8041c637b67e948247ee4c2444b07a64677749
Petr Simek <petr.simek@gmail.com>
2007-05-21 20:14:18 +0000

    Fixed filtering of scheduled downtime in situation when host is in s.d. but not down.

--- srcExtension/chrome/nagioschecker/content/nagioschecker-parser.js
+++ srcExtension/chrome/nagioschecker/content/nagioschecker-parser.js
@@ -410,14 +410,36 @@ NCHParser.prototype = {
       if (ar[0]) {
       var viptr = ar[0].childNodes[1].childNodes;
       var lastHost="";
+      var lastHostDowntime="";
       for (var i = 2; i < viptr.length; i+=2) {
         if (viptr[i] instanceof HTMLTableRowElement) {
           var viptd = viptr[i].childNodes;
           if (viptd.length>1) {                
+
+            var host_downtime=false;
             var host    = getUglyNodeValue(viptd[1],[0,1,0,1,1,1,0,1,0,0]);
+            var icons    = getUglyNode(viptd[1],[0,1,0,3,1,1,0]);
+				try {
+			  for(var j=0;j<icons.childNodes.length;j++) {
+              if (icons.childNodes[j] instanceof HTMLTableCellElement) {
+                var ico = getUglyNode(icons.childNodes[j],[0,0]);
+                if (ico) {
+                  var tit = ico.getAttribute("alt");
+                  if (tit) {
+                    if (tit.match("scheduled downtime")) {
+                      host_downtime=true;
+                    }
+                  }
+                }
+              }
+			  }
+				}
+				catch (e) {}
+
+            if ((!host) && (lastHostDowntime)) {
+				host_downtime=true;
+            };
             if (!host) host=lastHost;
-
-
 
             var service = getUglyNodeValue(viptd[3],[0,0,0,0,0,1,0,1,0,0]);
 
@@ -464,6 +486,10 @@ NCHParser.prototype = {
             if ((sto) && (parseInt(sto[1])<parseInt(sto[2]))) {
               isSoft=true;
             }         
+			if (host_downtime) {
+				downtime=true;
+			}
+
 				    if ((status=="UNKNOWN") || (status=="WARNING") || (status=="CRITICAL")) {
               var tmpo ={"type":"s","host": host,"service":service,"status":this.toLower[status],"lastCheck":lastCheck,"durationSec":durationSec,"duration":duration,"attempt":attempt,"info":info,"acknowledged":acknowledged,"dischecks":dischecks,"disnotifs":disnotifs,"isSoft":isSoft,"downtime":downtime};
 	            this.problems[pos][this.toLower[status]].push(tmpo);
@@ -472,6 +498,9 @@ NCHParser.prototype = {
 					    }
 				    }
             lastHost=host;
+            if (host_downtime) {
+            	lastHostDowntime=true;
+            }
           }
         }
       }
