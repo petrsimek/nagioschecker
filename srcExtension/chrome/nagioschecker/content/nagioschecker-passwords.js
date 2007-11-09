716c91600b49c568ed3de87653ed42a3ae30655a
Petr Simek <petr.simek@gmail.com>
2007-11-09 21:34:12 +0000

    Some testing of FF3 feature

--- srcExtension/chrome/nagioschecker/content/nagioschecker-passwords.js
+++ srcExtension/chrome/nagioschecker/content/nagioschecker-passwords.js
@@ -10,8 +10,14 @@ NCHPass.prototype = {
   cleanAuth : function(pos) {
     var ret = {"user":null,"password":null};
 
-    var passwordManager = Components.classes["@mozilla.org/passwordmanager;1"]
-                                .getService(Components.interfaces.nsIPasswordManager);
+var CC_passwordManager = Components.classes["@mozilla.org/passwordmanager;1"];
+var CC_loginManager = Components.classes["@mozilla.org/login-manager;1"];
+     
+if (CC_passwordManager != null) {
+   // Password Manager exists so this is not Firefox 3 (could be Firefox 2, Netscape, SeaMonkey, etc).
+   // Password Manager code
+   
+    var passwordManager = CC_passwordManager.getService(Components.interfaces.nsIPasswordManager);
 
     this.e = passwordManager.enumerator;
 
@@ -24,12 +30,27 @@ NCHPass.prototype = {
       } catch (ex) {
       }
     }
+   
+   
+}
+else if (CC_loginManager!= null) {
+   // Login Manager exists so this is Firefox 3
+   // Login Manager code
+   
+}
+
+
   },
   saveAuth : function(username,password,pos) {
     var ret = {"user":null,"password":null};
 
-    var passwordManager = Components.classes["@mozilla.org/passwordmanager;1"]
-                                .getService(Components.interfaces.nsIPasswordManager);
+
+var CC_passwordManager = Components.classes["@mozilla.org/passwordmanager;1"];
+var CC_loginManager = Components.classes["@mozilla.org/login-manager;1"];
+     
+if (CC_passwordManager != null) {
+
+    var passwordManager = CC_passwordManager.getService(Components.interfaces.nsIPasswordManager);
 
     this.e = passwordManager.enumerator;
 
@@ -43,11 +64,31 @@ NCHPass.prototype = {
       }
     }
       passwordManager.addUser('nagioschecker-url-'+pos, username, password);
+
+
+}
+else if (CC_loginManager!= null) {
+
+ var authLoginInfo = new nsLoginInfo('nagioschecker-url-'+pos,
+                       null, '',
+                       username, password, null, null);
+	CC_loginManager.addLogin(authLoginInfo);
+
+	
+}
+
   },
   getAuth : function(pos) {
     var ret = {"user":null,"password":null};
-    var passwordManager = Components.classes["@mozilla.org/passwordmanager;1"]
-                                .getService(Components.interfaces.nsIPasswordManager);
+
+var CC_passwordManager = Components.classes["@mozilla.org/passwordmanager;1"];
+var CC_loginManager = Components.classes["@mozilla.org/login-manager;1"];
+     
+if (CC_passwordManager != null) {
+   // Password Manager exists so this is not Firefox 3 (could be Firefox 2, Netscape, SeaMonkey, etc).
+   // Password Manager code
+   
+    var passwordManager = CC_passwordManager.getService(Components.interfaces.nsIPasswordManager);
 
     this.e = passwordManager.enumerator;
 
@@ -60,6 +101,26 @@ NCHPass.prototype = {
       } catch (ex) {
       }
     }
+
+   
+}
+else if (CC_loginManager!= null) {
+   // Login Manager exists so this is Firefox 3
+   // Login Manager code
+   
+   
+   var loginManager = CC_loginManager.getService(Components.interfaces.nsILoginManager);
+      // Find users for the given parameters
+   var logins = loginManager.findLogins({}, "nagioschecker-url-"+pos, null, '');
+  if (logins[0]) {
+alert(logins[0].username);
+  	ret.user = logins[0].username;
+  	ret.password = logins[0].password;
+  }       
+   
+}
+
+
 
     return ret;
   }
