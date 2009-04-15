e1d0a96eee98c6dc253f8d60363f2a6ce7ca0971
Petr Simek <petr.simek@gmail.com>
2009-04-15 08:41:38 +0000

    Fixed bug in creating status.cgi url

--- srcExtension/chrome/nagioschecker/content/utils.js
+++ srcExtension/chrome/nagioschecker/content/utils.js
@@ -72,7 +72,10 @@ function createNagiosUrl(server,type,host,service) {
 			params.push('servicegroup='+escape(parsed.queryKey['servicegroup']));
 			params.push('style=detail');			
 		}
-		else params.push('host=all');
+		
+		if (type=='host_problems') params.push('hostgroup=all');
+		else if (type=='service_problems') params.push('host=all');
+		
 	}
 	else url = url.replace(/status\.cgi/,'extinfo.cgi');
 	
